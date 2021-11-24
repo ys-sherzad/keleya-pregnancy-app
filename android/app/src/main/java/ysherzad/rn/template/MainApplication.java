7f478be5e850004b392b4b357eaac107d7a183c0
YasirSherzad <ys.sherzad@gmail.com>
2021-11-24 23:23:35 +0430

    rename to template

--- android/app/src/main/java/ysherzad/rn/template/MainApplication.java
+++ android/app/src/main/java/ysherzad/rn/template/MainApplication.java
@@ -1,4 +1,4 @@
-package com.foxie;
+package ysherzad.rn.template;
 
 import android.app.Application;
 import android.content.Context;
@@ -71,7 +71,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.foxie.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("ysherzad.rn.template.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
