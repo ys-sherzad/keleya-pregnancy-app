7e83e650d688b2fa0909885806edb9e9d7d388d2
YasirSherzad <ys.sherzad@gmail.com>
2021-12-09 19:01:51 +0430

    add theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+    white: '#fff',
+    pale_teal: '#69c0ba',
+    light_teal: '#9adcd7',
+    greyish_brown: '#4a4a4a',
+    warm_grey: '#9b9b9b',
+    bubble_gum: '#e97db5'
+};
