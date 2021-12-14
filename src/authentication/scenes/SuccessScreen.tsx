c6060ef8e50f1b7f19da54789d75de7c364a4e3f
YasirSherzad <ys.sherzad@gmail.com>
2021-12-14 21:11:13 +0430

    implement Success screen design

--- src/authentication/scenes/SuccessScreen.tsx
+++ src/authentication/scenes/SuccessScreen.tsx
@@ -1,20 +1,87 @@
 import * as React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import { RouterStackParamList, Screen } from '../types';
+import { theme } from '../../utils/theme';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Layout from '../components/Layout';
+import Title from '../components/Title';
+import Space from '../../shared/Space';
+import Button from '../../shared/Button';
 
 type SuccessScreenProps = StackScreenProps<RouterStackParamList, Screen.SuccessScreen>;
 
-const SuccessScreen = (props: SuccessScreenProps) => {
+const SuccessScreen = ({ }: SuccessScreenProps) => {
+
     return (
-        <View style={styles.container}>
-            <Text>SuccessScreen</Text>
-        </View>
+        <SafeAreaView
+            edges={['left', 'right', 'bottom']}
+            testID='sign_up_screen'
+            style={styles.container}
+        >
+            <Image
+                source={require('../../../assets/images/notifications-background-image.jpg')}
+                style={styles.backgroundImage}
+            />
+
+            <View style={styles.topSection}>
+                <Text>ICON</Text>
+
+                <Space size={20} />
+
+                <Title
+                    title='Get notifications to boost your motivations'
+                />
+            </View>
+
+            <View style={styles.bottomSection}>
+
+                <Button
+                    onPress={() => { }}
+                    title='Skip'
+                    customTextStyle={{
+                        fontSize: 16,
+                        fontWeight: '400'
+                    }}
+                />
+
+                <Space size={20} />
+
+                <Button
+                    onPress={() => { }}
+                    title='Continue'
+                    primary
+                />
+            </View>
+        </SafeAreaView>
     );
 };
 
 export default SuccessScreen;
 
 const styles = StyleSheet.create({
-    container: {}
+    flexOne: {
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        backgroundColor: theme.white,
+    },
+    backgroundImage: {
+        width: '100%',
+        resizeMode: 'cover',
+    },
+    topSection: {
+        position: 'absolute',
+        top: 90,
+        width: '100%',
+        paddingHorizontal: 50,
+        alignItems: 'center'
+    },
+    bottomSection: {
+        position: 'absolute',
+        bottom: 35,
+        width: '100%',
+        paddingHorizontal: 50,
+    }
 });
