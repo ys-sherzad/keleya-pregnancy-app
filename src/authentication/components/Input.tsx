17c0c20d2a96c5549e9515c69e3a4a97ff18d2d5
YasirSherzad <ys.sherzad@gmail.com>
2021-12-15 18:02:53 +0430

    refactor Input component

--- src/authentication/components/Input.tsx
+++ src/authentication/components/Input.tsx
@@ -1,29 +1,22 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TextInput, StyleProp, TextStyle } from 'react-native';
+import { View, StyleSheet, TextInput, StyleProp, TextStyle, TextInputProps } from 'react-native';
 import { theme } from '../../utils/theme';
 import EyeOn from '../../../assets/icons/eye-on.svg';
 import EyeOff from '../../../assets/icons/eye-off.svg';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Space from '../../shared/Space';
 
-interface InputProps {
-  placeholder: string;
-  onChangeText: (text: string) => void;
-  value: string;
+type InputProps = React.ComponentProps<typeof TextInput> & {
   isPassword?: boolean;
-  testID?: string;
   customInputStyle?: StyleProp<TextStyle>;
   customInputContainerStyle?: StyleProp<TextStyle>;
-}
+};
 
 const Input = ({
-  placeholder,
-  onChangeText,
-  value,
   customInputStyle,
   customInputContainerStyle,
-  testID,
   isPassword = false,
+  ...rest
 }: InputProps) => {
 
   // The state for showing/not showing text characters
@@ -32,14 +25,11 @@ const Input = ({
   return (
     <View style={[styles.container, customInputContainerStyle]}>
       <TextInput
-        testID={testID}
-        placeholder={placeholder}
-        value={value}
-        onChangeText={onChangeText}
         style={[styles.defaultInputStyle, customInputStyle]}
         secureTextEntry={isTextSecure}
         placeholderTextColor={theme.warm_grey}
         selectionColor={theme.greyish_brown}
+        {...rest}
       />
 
       {isPassword && (
