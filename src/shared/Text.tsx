3c586bb0916c09e9f6749255b718b3a6761e2f11
YasirSherzad <ys.sherzad@gmail.com>
2021-12-15 13:44:20 +0430

    add Text component

---
+++ src/shared/Text.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Text as RNText, StyleSheet, StyleProp, TextStyle } from 'react-native';
+
+type TextProps = React.ComponentProps<typeof RNText> & {
+    children: React.ReactNode;
+    style?: StyleProp<TextStyle>;
+};
+
+const Text = ({
+    children,
+    style,
+    ...rest
+}: TextProps) => {
+    return (
+        <RNText
+            style={[styles.defaultStyle, style]}
+            {...rest}
+        >
+            {children}
+        </RNText>
+    );
+};
+
+export default Text;
+
+const styles = StyleSheet.create({
+    defaultStyle: {
+        fontFamily: 'Raleway'
+    }
+});
