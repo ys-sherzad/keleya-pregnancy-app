36b089307b567aef5c13a9335bbe8c8b46b2e87b
YasirSherzad <ys.sherzad@gmail.com>
2021-12-18 14:06:27 +0430

    fix Dimensions file name

--- src/utils/Dimensions.ts
+++ src/utils/Dimensions.ts
@@ -1,4 +1,5 @@
 import { Dimensions } from 'react-native';
 
+// screen dimensions
 export const SCR_HEIGHT = Dimensions.get('window').height;
 export const SCR_WIDTH = Dimensions.get('window').width;
