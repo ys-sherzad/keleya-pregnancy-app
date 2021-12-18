7fee986c5b328c5708beb2de7bda864ddc305705
YasirSherzad <ys.sherzad@gmail.com>
2021-12-18 14:09:31 +0430

    delete dimensions file

--- src/utils/Dimensions.ts
+++
@@ -1,5 +0,0 @@
-import { Dimensions } from 'react-native';
-
-// screen dimensions
-export const SCR_HEIGHT = Dimensions.get('window').height;
-export const SCR_WIDTH = Dimensions.get('window').width;
